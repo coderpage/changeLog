5e902bb7a4bf73b881522a36d8518931d915b937
liuchao0206 <liuchao0206@gmail.com>
2015-08-13 19:47:49 +0800

    读取 log.properties 内容

--- lib/log.sh
+++ lib/log.sh
@@ -7,15 +7,34 @@ pipe_temp_file=temp.txt
 log_file=changelog.md
 log_file_tag=changelog.txt
 
-repos_url=https://github.com/coderpage/changeLog
+repos_url=
 
 
 rm -f ${log_file}
 rm -f ${log_file_temp}
 
+# 获取当前分支名称
 current_branch=`git branch | sed -e '/^[^*]/d' -e 's/^*//'`
 
+# 获取 log.properties 配置文件内容
+while read properties
+    do
+        if [ "x${properties}" == "x" ]; then
+            continue
+        fi
 
+        name=`echo ${properties} | cut -d = -f 1`
+        value=`echo ${properties} | cut -d = -f 2`
+
+        case "${name}" in
+
+        "repository" ) repos_url=${value};;
+
+        esac
+
+    done < log.properties
+
+# 读取当前 tag 下的 changelog.txt 内容，并写入 changelog.md
 function readLog
 {
     echo "#### 更新记录" >> ${log_file_temp}
@@ -41,7 +60,7 @@ function readLog
     echo '    ' >> ${log_file_temp}
 }
 
-
+# 遍历所有 tag，获取 commit 信息
 git for-each-ref --sort='*authordate' --format='%(tag)' refs/tags | grep -v '^$' | tail -r > ${pipe_temp_file}
 while read TAG ; do
 
@@ -84,10 +103,13 @@ done<${pipe_temp_file}
     GIT_PAGER=cat git log --no-merges --date=short  --pretty=format:'- %ad (%an) %s -> [view commit](${repos_url}/%H)' $current_tag >> ${log_file_temp}
     echo '    ' >> ${log_file_temp}
 
-
+# 切换回工作分支
 git checkout $current_branch
 
+# 将 temp 文件内容拷贝到 changelog.md
 cp ${log_file_temp} ${log_file}
+
+# 删除临时文件
 rm -f ${log_file_temp}
 rm -f ${pipe_temp_file}
 
