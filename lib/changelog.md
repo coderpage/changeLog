27ce1a90044b2d11cdeca3a29a1eb4d40a33d48d
liuchao0206 <liuchao0206@gmail.com>
2015-08-20 18:36:52 +0800

    修改changelog生成方式

---
+++ lib/changelog.md
@@ -0,0 +1,15 @@
+    
+---
+## 
+#### 更新记录
+    
+*此版本没有设置 tag 更新记录文件*
+    
+    
+#### commit 记录
+    
+- 2015-08-13 (liuchao0206) 读取 log.properties 内容 -> [view commit](https://github.com/coderpage/changeLog/commit/5e902bb7a4bf73b881522a36d8518931d915b937)
+- 2015-08-13 (liuchao0206) 添加 example -> [view commit](https://github.com/coderpage/changeLog/commit/9d7d81fc985c8736377eff8fab2db48a7de85a7a)
+- 2015-08-13 (liuchao0206) 添加 README.md，修该log.sh -> [view commit](https://github.com/coderpage/changeLog/commit/899c6e4a4a2c6773b23257235d56585f7c73d0fc)
+- 2015-08-10 (liuchao0206) 初始化仓库 -> [view commit](https://github.com/coderpage/changeLog/commit/9cad9e1adff8639bdd7a2113da6f3a0e4140c38a)
+- 2015-08-10 (刘超) Initial commit -> [view commit](https://github.com/coderpage/changeLog/commit/14dfdad6acb23d1cc15bcb887c203d5a414f118c)    
--- lib/log.sh
+++ lib/log.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #Author: Abner-L 2015-08-10
 
-log_file_temp=log_temp.md
-pipe_temp_file=temp.txt
+log_file_temp=tmp/log_tmp.md
+pipe_temp_file=tmp/tmp.txt
 
 log_file=changelog.md
-log_file_tag=changelog.txt
+log_file_tag_dir=tag_log
 
+# 仓库远程 url，用于生成 commit 信息路径
 repos_url=
-
 
 rm -f ${log_file}
 rm -f ${log_file_temp}
+mkdir tmp
 
 # 获取当前分支名称
-current_branch=`git branch | sed -e '/^[^*]/d' -e 's/^*//'`
+#current_branch=`git branch | sed -e '/^[^*]/d' -e 's/^*//'`
 
 # 获取 log.properties 配置文件内容
 while read properties
@@ -26,6 +27,7 @@ while read properties
         name=`echo ${properties} | cut -d = -f 1`
         value=`echo ${properties} | cut -d = -f 2`
 
+
         case "${name}" in
 
         "repository" ) repos_url=${value};;
@@ -35,18 +37,44 @@ while read properties
     done < log.properties
 
 # 读取当前 tag 下的 changelog.txt 内容，并写入 changelog.md
-function readLog
+#function readLog
+# {
+#    echo "#### 更新记录" >> ${log_file_temp}
+#    echo '    ' >> ${log_file_temp}
+#
+#    if [ ! -f ${log_file_tag} ];then
+#
+#    echo '*此版本没有设置 changelog.txt*' >> ${log_file_temp}
+#
+#    else
+#
+#    cat ${log_file_tag} | while read LINE
+#    do
+#        if [ "x${LINE}" == "x" ]
+#            then
+#            continue
+#        fi
+#
+#        echo "- $LINE" >> ${log_file_temp}
+#    done
+#
+#    fi
+#    echo '    ' >> ${log_file_temp}
+#}
+
+# 读取每个 tag 更新记录文件内容，写入 changelog.md
+function read_tag
 {
     echo "#### 更新记录" >> ${log_file_temp}
     echo '    ' >> ${log_file_temp}
 
-    if [ ! -f ${log_file_tag} ];then
+    if [ ! -f ${log_file_tag_dir}/$1 ];then
 
-    echo '*此版本没有设置 changelog.txt*' >> ${log_file_temp}
+    echo '*此版本没有设置 tag 更新记录文件*' >> ${log_file_temp}
 
     else
 
-    cat ${log_file_tag} | while read LINE
+    cat  ${log_file_tag_dir}/$1 | while read LINE
     do
         if [ "x${LINE}" == "x" ]
             then
@@ -68,50 +96,48 @@ while read TAG ; do
 
         echo '    ' >> ${log_file_temp}
         echo '---' >> ${log_file_temp}
-        echo "## ${NEXT}"  >> ${log_file_temp}
-        readLog
+        echo "## ${NEXT}" >> ${log_file_temp}
+        read_tag ${NEXT}
         echo '    ' >> ${log_file_temp}
         echo '#### commit 记录' >> ${log_file_temp}
         echo '    ' >> ${log_file_temp}
-        GIT_PAGER=cat git log --no-merges --date=short  --pretty=format:'- %ad (%an) %s -> [view commit](${repos_url}/commit/%H)' $TAG..$NEXT >> ${log_file_temp}
+        GIT_PAGER=cat git log --no-merges --date=short  --pretty=format:'- %ad (%an) %s -> [view commit]('${repos_url}'/commit/%H)' $TAG..$NEXT >> ${log_file_temp}
         echo '    ' >> ${log_file_temp}
 
     else
 
-        echo "## 当前" >> ${log_file_temp}
-        readLog
+        echo "## 当前"  >> ${log_file_temp}
+        read_tag current
         echo '#### commit 记录' >> ${log_file_temp}
         echo '    ' >> ${log_file_temp}
-        GIT_PAGER=cat git log --no-merges --date=short  --pretty=format:'- %ad (%an) %s -> [view commit](${repos_url}/commit/%H)' $TAG.. >> ${log_file_temp}
+        GIT_PAGER=cat git log --no-merges --date=short  --pretty=format:'- %ad (%an) %s -> [view commit]('${repos_url}'/commit/%H)' $TAG.. >> ${log_file_temp}
         echo '    ' >> ${log_file_temp}
 
     fi
 
-    git checkout ${TAG}
+#    git checkout ${TAG}
     NEXT=${TAG}
     current_tag=$TAG
 done<${pipe_temp_file}
 
-    echo $current_tag
     echo '    ' >> ${log_file_temp}
     echo '---' >> ${log_file_temp}
     echo "## "$current_tag  >> ${log_file_temp}
-    readLog
+    read_tag ${current_tag}
     echo '    ' >> ${log_file_temp}
     echo '#### commit 记录' >> ${log_file_temp}
     echo '    ' >> ${log_file_temp}
-    GIT_PAGER=cat git log --no-merges --date=short  --pretty=format:'- %ad (%an) %s -> [view commit](${repos_url}/%H)' $current_tag >> ${log_file_temp}
+    GIT_PAGER=cat git log --no-merges --date=short  --pretty=format:'- %ad (%an) %s -> [view commit]('${repos_url}'/commit/%H)' $current_tag >> ${log_file_temp}
     echo '    ' >> ${log_file_temp}
 
 # 切换回工作分支
-git checkout $current_branch
+#git checkout $current_branch
 
 # 将 temp 文件内容拷贝到 changelog.md
 cp ${log_file_temp} ${log_file}
 
 # 删除临时文件
-rm -f ${log_file_temp}
-rm -f ${pipe_temp_file}
+rm -r tmp
 
 echo "DONE."
 
